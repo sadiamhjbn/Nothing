6a08218015b010131d6eea93c652f9f37c062cfb
sadiamhjbn <sadiamhjbn@gmail.com>
2019-08-02 14:35:25 +0600

    collpse button doesn't work

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,6 +1,5 @@
-
-import React, { Component } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import React, {Component} from 'react';
+import {TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col} from 'reactstrap';
 import 'whatwg-fetch';
 import {
   getFromStorage,
@@ -12,86 +11,92 @@ class Home extends Component {
     super(props);
 
     this.state = {
-      isLoading : true,
+      isLoading: true,
       token: '',
       signUpError: '',
       signInError: '',
       signInEmail: '',
-      signInPassword:'',
-      signUpFirstName:'',
-      signUpLastName:'',
-      signUpEmail:'',
-      signUpPassword:'',
+      signInPassword: '',
+      signUpFirstName: '',
+      signUpLastName: '',
+      signUpEmail: '',
+      signUpPassword: '',
       activeTab: '1',
     };
     this.onTextBoxChangeSignInEmail = this.onTextBoxChangeSignInEmail.bind(this);
     this.onTextBoxChangeSignInPassword = this.onTextBoxChangeSignInPassword.bind(this);
-    this.onTextBoxChangeSignUpEmail=this.onTextBoxChangeSignUpEmail.bind(this);
+    this.onTextBoxChangeSignUpEmail = this.onTextBoxChangeSignUpEmail.bind(this);
     this.onTextBoxChangeSignUpPassword = this.onTextBoxChangeSignUpPassword.bind(this);
-    this.onTextBoxChangeSignUpFirstName=this.onTextBoxChangeSignUpFirstName.bind(this);
-    this.onTextBoxChangeSignUpLastName=this.onTextBoxChangeSignUpLastName.bind(this);
+    this.onTextBoxChangeSignUpFirstName = this.onTextBoxChangeSignUpFirstName.bind(this);
+    this.onTextBoxChangeSignUpLastName = this.onTextBoxChangeSignUpLastName.bind(this);
     this.onSignUp = this.onSignUp.bind(this);
-    this.onSignIn= this.onSignIn.bind(this);
-    this.onLogOut= this.onLogOut.bind(this);
+    this.onSignIn = this.onSignIn.bind(this);
+    this.onLogOut = this.onLogOut.bind(this);
     this.toggle = this.toggle.bind(this);
   }
 
   componentWillMount() {
     const obj = getFromStorage('the_main_app');
-      if (obj && obj.token){
-        const {token} = obj;
-        //verify token
-        fetch('/api/account/verify?token='+token)
-          .then(res => res.json())
-          .then(json => {
-            if(json.success){
-              this.setState({
-                token,
-                isLoading: false,
-              });
-            }else{
-              this.setState({
-                isLoading: false,
-              });
-            }
-          });
-      }
-      else{
-        this.setState({
-          isLoading: false
+    if (obj && obj.token) {
+      const {token} = obj;
+      //verify token
+      fetch('/api/account/verify?token=' + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false,
+            });
+          } else {
+            this.setState({
+              isLoading: false,
+            });
+          }
         });
-      }
+    } else {
+      this.setState({
+        isLoading: false
+      });
+    }
   }
-  onTextBoxChangeSignInEmail(event){
+
+  onTextBoxChangeSignInEmail(event) {
     this.setState({
       signInEmail: event.target.value,
     });
   }
-  onTextBoxChangeSignInPassword(event){
+
+  onTextBoxChangeSignInPassword(event) {
     this.setState({
       signInPassword: event.target.value,
     });
   }
-  onTextBoxChangeSignUpEmail(event){
+
+  onTextBoxChangeSignUpEmail(event) {
     this.setState({
       signUpEmail: event.target.value,
     });
   }
-  onTextBoxChangeSignUpPassword(event){
+
+  onTextBoxChangeSignUpPassword(event) {
     this.setState({
       signUpPassword: event.target.value,
     });
   }
-  onTextBoxChangeSignUpFirstName(event){
+
+  onTextBoxChangeSignUpFirstName(event) {
     this.setState({
       signUpFirstName: event.target.value,
     });
   }
-  onTextBoxChangeSignUpLastName(event){
+
+  onTextBoxChangeSignUpLastName(event) {
     this.setState({
       signUpLastName: event.target.value,
     });
   }
+
   toggle(tab) {
     if (this.state.activeTab !== tab) {
       this.setState({
@@ -99,9 +104,10 @@ class Home extends Component {
       });
     }
   }
-  onSignUp(){
+
+  onSignUp() {
     //grab state
-    const{
+    const {
       signUpFirstName,
       signUpLastName,
       signUpEmail,
@@ -125,16 +131,16 @@ class Home extends Component {
     })
       .then(res => res.json())
       .then(json => {
-        if(json.success){
+        if (json.success) {
           this.setState({
             signUpError: json.message,
             isLoading: false,
-            signUpEmail :'',
-            signUpPassword:'',
-            signUpFirstName:'',
-            signUpLastName:'',
+            signUpEmail: '',
+            signUpPassword: '',
+            signUpFirstName: '',
+            signUpLastName: '',
           });
-        } else{
+        } else {
           this.setState({
             signUpError: json.message,
             isLoading: false,
@@ -145,9 +151,9 @@ class Home extends Component {
 
   }
 
-  onSignIn(){
+  onSignIn() {
     //grab state
-    const{
+    const {
       signInEmail,
       signInPassword,
     } = this.state;
@@ -167,17 +173,16 @@ class Home extends Component {
     })
       .then(res => res.json())
       .then(json => {
-        if(json.success){
-          setInStorage('the_main_app',{token:json.token});
+        if (json.success) {
+          setInStorage('the_main_app', {token: json.token});
           this.setState({
             signInError: json.message,
             isLoading: false,
-            signInEmail :'',
-            signInPassword:'',
+            signInEmail: '',
+            signInPassword: '',
             token: json.token,
           });
-        }
-        else{
+        } else {
           this.setState({
             signInError: json.message,
             isLoading: false,
@@ -187,30 +192,30 @@ class Home extends Component {
       });
 
   }
-  onLogOut(){
+
+  onLogOut() {
     this.setState({
       isLoading: true,
     });
     const obj = getFromStorage('the_main_app');
-    if (obj && obj.token){
+    if (obj && obj.token) {
       const {token} = obj;
       //verify token
-      fetch('/api/account/logout?token='+token)
+      fetch('/api/account/logout?token=' + token)
         .then(res => res.json())
         .then(json => {
-          if(json.success){
+          if (json.success) {
             this.setState({
-              token:'',
+              token: '',
               isLoading: false,
             });
-          }else{
+          } else {
             this.setState({
               isLoading: false,
             });
           }
         });
-    }
-    else{
+    } else {
       this.setState({
         isLoading: false
       });
@@ -240,7 +245,7 @@ class Home extends Component {
   }
 
   render() {
-    const{
+    const {
       isLoading,
       token,
       signInError,
@@ -251,30 +256,34 @@ class Home extends Component {
       signUpEmail,
       signUpPassword,
       signUpError,
-    }=this.state;
-    if(isLoading){
+    } = this.state;
+    if (isLoading) {
       return (<div><p>Loading...</p></div>);
     }
 
-    if(!token){
+    if (!token) {
       return (
         <div>
 
           <div className="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2 mt-5 card">
-            <div >
+            <div>
               <Nav tabs className="d-flex justify-content-center">
                 <NavItem className="text-center">
                   <NavLink
-                    className={this.state.activeTab === '1'? "active":""}
-                    onClick={() => { this.toggle('1'); }}
+                    className={this.state.activeTab === '1' ? "active" : ""}
+                    onClick={() => {
+                      this.toggle('1');
+                    }}
                   >
                     Sign In
                   </NavLink>
                 </NavItem>
                 <NavItem className="text-center">
                   <NavLink
-                    className={this.state.activeTab === '2'? "active":""}
-                    onClick={() => { this.toggle('2'); }}
+                    className={this.state.activeTab === '2' ? "active" : ""}
+                    onClick={() => {
+                      this.toggle('2');
+                    }}
                   >
                     Sign Up
                   </NavLink>
@@ -284,19 +293,20 @@ class Home extends Component {
             <br/>
             <TabContent activeTab={this.state.activeTab}>
               <TabPane tabId="1">
-                <form >
+                <form>
                   {
-                    (signInError)?(
+                    (signInError) ? (
                       <div className="text-center">
                         <p className="text-danger">{signInError}</p>
                       </div>
-                    ): (null)
+                    ) : (null)
                   }
                   <div className="form-group row">
                     <label htmlFor="email" className=" col-4 text-right col-form-label">Email address</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="email" placeholder="Email" value={signInEmail} onChange={this.onTextBoxChangeSignInEmail}
+                        className="form-control" type="email" placeholder="Email" value={signInEmail}
+                        onChange={this.onTextBoxChangeSignInEmail}
                       />
                     </div>
                   </div>
@@ -305,7 +315,8 @@ class Home extends Component {
                     <label htmlFor="password" className=" col-4 text-right col-form-label">Password</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="password" placeholder="Password" value={signInPassword} onChange={this.onTextBoxChangeSignInPassword}
+                        className="form-control" type="password" placeholder="Password" value={signInPassword}
+                        onChange={this.onTextBoxChangeSignInPassword}
                       />
                     </div>
                   </div>
@@ -321,17 +332,18 @@ class Home extends Component {
               <TabPane tabId="2">
                 <form>
                   {
-                    (signUpError)?(
+                    (signUpError) ? (
                       <div className="text-center">
                         <p className="text-danger">{signUpError}</p>
                       </div>
-                    ): (null)
+                    ) : (null)
                   }
                   <div className="form-group row">
                     <label htmlFor="firstname" className=" col-4 text-right col-form-label">First Name</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="text" placeholder="First Name" value={signUpFirstName} onChange={this.onTextBoxChangeSignUpFirstName}
+                        className="form-control" type="text" placeholder="First Name" value={signUpFirstName}
+                        onChange={this.onTextBoxChangeSignUpFirstName}
                       />
                     </div>
                   </div>
@@ -340,7 +352,8 @@ class Home extends Component {
                     <label htmlFor="lastname" className=" col-4 text-right col-form-label">Last Name</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="text" placeholder="Last Name" value={signUpLastName} onChange={this.onTextBoxChangeSignUpLastName}
+                        className="form-control" type="text" placeholder="Last Name" value={signUpLastName}
+                        onChange={this.onTextBoxChangeSignUpLastName}
                       />
                     </div>
                   </div>
@@ -349,7 +362,8 @@ class Home extends Component {
                     <label htmlFor="email" className=" col-4 text-right col-form-label">Email Address</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="email" placeholder="Email" value={signUpEmail} onChange={this.onTextBoxChangeSignUpEmail}
+                        className="form-control" type="email" placeholder="Email" value={signUpEmail}
+                        onChange={this.onTextBoxChangeSignUpEmail}
                       />
                     </div>
                   </div>
@@ -358,14 +372,15 @@ class Home extends Component {
                     <label htmlFor="password" className=" col-4 text-right col-form-label">Password</label>
                     <div className="col-6">
                       <input
-                        className="form-control" type="password" placeholder="Password" value={signUpPassword} onChange={this.onTextBoxChangeSignUpPassword}
+                        className="form-control" type="password" placeholder="Password" value={signUpPassword}
+                        onChange={this.onTextBoxChangeSignUpPassword}
                       />
                     </div>
                   </div>
                   <br/>
                   <div className=" form-group row">
                     <div className="offset-4 col-4">
-                      <button className="btn btn-outline-greenish" onClick={this.onSignUp} >Sign Up</button>
+                      <button className="btn btn-outline-greenish" onClick={this.onSignUp}>Sign Up</button>
                     </div>
                   </div>
                 </form>
@@ -376,10 +391,38 @@ class Home extends Component {
       )
     }
     return (
-        <div>
-            <p>Account</p>
-          <button className="btn btn-outline-info offset-6 " onClick={this.onLogOut}>Log Out</button>
-        </div>
+      <div>
+        <nav className="navbar navbar-expand-lg bg-greenish navbar-dark sticky-top">
+          <a className="navbar-brand" href="#">
+            <h4 className="text-center text-gallery title">Home</h4>
+          </a>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link " href="#">Enrolled Courses</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href="#">More Courses</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href="#">Manage account</a>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-greenish" onClick={this.onLogOut}>Log out</button>
+              </li>
+
+            </ul>
+          </div>
+          <form action="" className="justify-content-end form-inline ">
+            <input className="form-control bg-transparent" placeholder="Search  " type="text"/>
+          </form>
+        </nav>
+      </div>
     );
   }
 }
