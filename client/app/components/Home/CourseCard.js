3914a7530481821c8a6c76624b7f5dd5a0e2e518
sadiamhjbn <sadiamhjbn@gmail.com>
2019-08-04 01:24:08 +0600

    bar text edited

--- client/app/components/Home/CourseCard.js
+++ client/app/components/Home/CourseCard.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import {Card, CardBody, CardFooter, CardHeader, CardText, CardTitle, Col, Progress, Row} from "reactstrap";
-export default class CourseCard extends Component{
-  render(){
-    const progress = this.props.completed/this.props.total*100;
-    let color, progresscolor;
-    if(progress===100) {color="complete";
-    progresscolor ="dark";
+
+export default class CourseCard extends Component {
+  render() {
+    const progress = this.props.completed / this.props.total * 100;
+    let color, progressColor;
+    if (progress === 100) {
+      color = "complete";
+      progressColor = "dark";
+    } else if (progress === 0) {
+      color = "enrolled";
+      progressColor = "warning";
+    } else {
+      color = "continuous";
+      progressColor = "info";
     }
-    else if(progress===0){
-      color="enrolled";
-      progresscolor ="warning";
-    }
-    else{
-      color="continuous";
-      progresscolor ="info";
-    }
-    return(
-      <Col lg="3" sm="6">
+    return (
+      <Col lg="3" md="4" sm="6" >
         <Card color={color} className="text-white mt-3">
           <CardHeader><h3><CardTitle>{this.props.title}</CardTitle></h3></CardHeader>
           <CardBody className="lead">
@@ -25,7 +25,8 @@ export default class CourseCard extends Component{
             <CardText>Completed Assignment: <span className="text-monospace">{this.props.completed}</span></CardText>
           </CardBody>
           <CardFooter>
-            <Progress className="text-darkgreen" value={progress} color={progresscolor} style={{height:'1.5rem'}}>{progress}%</Progress>
+            <Progress barClassName={progress ? "text-white" : "text-dark"} value={progress} color={progressColor}
+                      style={{height: '1.5rem'}}>{progress}%</Progress>
           </CardFooter>
         </Card>
       </Col>
