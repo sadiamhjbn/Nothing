fbb34695193b497f147a49aab9f4034fcea7e942
sadiamhjbn <sadiamhjbn@gmail.com>
2019-08-17 00:44:21 +0600

    more courses loaded from server

--- client/app/components/Home/MoreCourses.js
+++ client/app/components/Home/MoreCourses.js
@@ -1,30 +1,33 @@
 import React, {Component} from 'react';
 import MoreCourseCard from "./MoreCourseCard";
-import {
-  Nav, NavItem, Row, Collapse, Navbar, NavbarToggler, NavbarBrand, Form
-} from 'reactstrap';
+import {getFromStorage} from "../../utils/storage";
+import axios from "axios";
 
 export default class MoreCourses extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: undefined,
+      moreCourses: [],
+    }
+  }
+
+  componentDidMount() {
+    const cache = getFromStorage('the_main_app');
+    if (!cache || !cache.token) {
+      return;
+    }
+    const token = cache.token;
+    this.setState({token});
+    axios.get(`/api/courses/more?token=${token}`)
+      .then(response => {
+        this.setState({moreCourses: response.data.moreCourses});
+      });
+  }
   render() {
-    return (
-      <>
-        <MoreCourseCard duration="34:12" total="6" category="JavaScript" title="Course Name"/>
-        <MoreCourseCard
-          duration="40:00"
-          total="6"
-          category="JavaScript"
-          title="Course Name"/>
-        <MoreCourseCard
-          duration="49:00"
-          total="6"
-          category="JavaScript"
-          title="Course Name"/>
-        <MoreCourseCard
-          duration="40:00"
-          total="6"
-          category="JavaScript"
-          title="Course Name"/>
-      </>
-    )
+    return this.state.moreCourses.map(course => {
+      return <MoreCourseCard key={course.title} duration={course.duration} total={course.total} category={course.category} title={course.title}/>;
+    });
   }
 }
--- server/routes/api/courseRouter.js
+++ server/routes/api/courseRouter.js
@@ -36,6 +36,41 @@ function Router(app) {
         response.status(401).send({message: err.message});
       })
   });
+  app.get('/api/courses/more', function (request, response) {
+    const token = request.query.token;
+
+    verify(token)
+      .then(() => {
+        let moreCourses = [{
+          title: 'Course Title 1',
+          duration: '34:12',
+          category: 'JS',
+          total: 3
+        }, {
+          title: 'Course Title 2',
+          duration: '39:10',
+          category: 'PHP',
+          total: 6
+        },{
+          title: 'Course Title 3',
+          duration: '14:12',
+          category: 'JS',
+          total: 0
+        },{
+          title: 'Course Title 4',
+          duration: '54:12',
+          category: 6,
+          total: 1
+        }];
+
+        response.send({
+          moreCourses: moreCourses
+        })
+      })
+      .catch(err => {
+        response.status(401).send({message: err.message});
+      })
+  });
 }
 
 module.exports = Router;
