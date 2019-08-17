f6e80c93ddd0c33ae565cf4a84a9485ebc983f07
sadiamhjbn <sadiamhjbn@gmail.com>
2019-08-17 21:02:13 +0600

    added some tabs in StartCourse

--- client/app/components/Home/StartCourse.js
+++ client/app/components/Home/StartCourse.js
@@ -1,12 +1,109 @@
 import React,{Component} from "react";
+import {Card, CardHeader, CardTitle, Col, Nav, NavItem, NavLink, Row, TabContent, TabPane} from "reactstrap";
+import classnames from 'classnames';
 
 export default class StartCourse extends Component{
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: '1'
+    };
+  }
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="embed-responsive embed-responsive-21by9 mt-3 row">
-        <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/sLe3xW0FN2M"
-                allowFullScreen>
-        </iframe>
+      <div className="col-12">
+        <div className="embed-responsive embed-responsive-16by9 mt-3 col-lg-10 offset-lg-1 col-md-12">
+          <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/sLe3xW0FN2M"
+                  allowFullScreen>
+          </iframe>
+        </div>
+        <div className="card m-3 bg-transparent">
+          <CardHeader>
+            <Nav tabs >
+              <NavItem className="col-3 text-center">
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '1' })}
+                  onClick={() => { this.toggle('1'); }}
+                >
+                  Course Contents
+                </NavLink>
+              </NavItem>
+              <NavItem className="col-3 text-center">
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '2' })}
+                  onClick={() => { this.toggle('2'); }}
+                >
+                  Overview
+                </NavLink>
+              </NavItem>
+              <NavItem className="col-3 text-center">
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '3' })}
+                  onClick={() => { this.toggle('3'); }}
+                >
+                  Q/A
+                </NavLink>
+              </NavItem >
+              <NavItem className="col-3 text-center">
+                <NavLink
+                  className={classnames({ active: this.state.activeTab === '4' })}
+                  onClick={() => { this.toggle('4'); }}
+                >
+                  Assignments
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </CardHeader>
+          <TabContent className="mt-2" activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <ul>
+                    <li>content1</li>
+                    <li>content2</li>
+                    <li>content3</li>
+                  </ul>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="12">
+                  <h5>Egypt, a country linking northeast Africa with the Middle East, dates to the time of the pharaohs. Millennia-old monuments sit along the fertile Nile River Valley, including Giza's colossal Pyramids and Great Sphinx as well as Luxor's hieroglyph-lined Karnak Temple and Valley of the Kings tombs. The capital, Cairo, is home to Ottoman landmarks like Muhammad Ali Mosque and the Egyptian Museum, a trove of antiquities.</h5>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="3">
+              <Row>
+                <Col sm="12">
+                  <ul>
+                    <li><strong>question1</strong></li>
+                    =>answer1
+                    <li><strong>question2</strong></li>
+                    =>answer2
+                  </ul>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="4">
+              <Row>
+                <Col sm="12">
+                  <h4>Tab 4 Contents</h4>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
+
       </div>
     );
   }
